bd01c449909d69b22b23f2c2b05bbe731cd0267e
estevanmontoya <montoya.estevan@gmail.com>
2017-03-02 14:04:33 -0700

    Added skeleton.css and about.pug

--- app.js
+++ app.js
@@ -1,20 +1,28 @@
 var express = require('express');
 var app = express();
 
-let siteName = 'Craft Faith';
+// serve static files from /public
+app.use(express.static(__dirname + '/public'));
 
 // view engine setup
 app.set('view engine', 'pug');
 app.set('views', __dirname + '/views');
 
-// GET '/' initial response
+// GET '/' page
 app.get('/', function(req, res) {
     res.render('index', {
-      title: "Craft Faith"
+      title: 'Craft Faith'
     });
-})
+});
+
+// GET '/about' page
+app.get('/about', function(req, res) {
+  res.render('about', {
+    title: 'Craft Faith'
+  });
+});
 
 // Set port and console.log
 app.listen(3000, function() {
-  console.log(`${siteName} is running locally at http://localhost:3000`);
+  console.log('Site is running locally at http://localhost:3000');
 });
