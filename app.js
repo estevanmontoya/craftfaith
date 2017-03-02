36385ec210b015188f6740eff44b99ab479ea2c6
estevanmontoya <montoya.estevan@gmail.com>
2017-03-02 13:33:06 -0700

    Added pug, views folder, and index.pug

--- app.js
+++ app.js
@@ -1,6 +1,20 @@
 var express = require('express');
 var app = express();
 
+let siteName = 'Craft Faith';
+
+// view engine setup
+app.set('view engine', 'pug');
+app.set('views', __dirname + '/views');
+
+// GET '/' initial response
+app.get('/', function(req, res) {
+    res.render('index', {
+      title: "Craft Faith"
+    });
+})
+
+// Set port and console.log
 app.listen(3000, function() {
-  console.log('Craft Faith is running locally at http://localhost:3000');
+  console.log(`${siteName} is running locally at http://localhost:3000`);
 });
